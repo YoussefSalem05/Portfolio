fa43a1bcb86edf18e0e7b1a390fe2d7df6e568c3
Youssef Salem <56196893+YoussefSalem05@users.noreply.github.com>
2026-04-01 21:41:00 +0200

    Update Study1.py

--- Coding/Python/Study1.py
+++ Coding/Python/Study1.py
@@ -1,1 +1,28 @@
+def main():
+    grades=None
+    while grades is None:
+        try:
+            temp=input("Enter list of grades separated by commas: ")
+            print("You entered:", temp)
+            temp=temp.split(",")
+            temp=[int(g) for g in temp]
+            if any(g<0 or g>100 for g in temp):
+                print("Grades must be between 0 and 100.")
+                continue
+            grades=temp
+        except ValueError:
+            print("Invalid range.")
+            grades=None
 
+    if len(grades)!=0:    
+     average=sum(grades)/len(grades) 
+     print("Average grade:", average)
+     if average>=90:
+            print("Status:Excellent")
+    elif average<=89 and average>=75:
+            print("Status:Good")
+    else:
+            print("Status:Needs Improvement")
+        
+if __name__ == '__main__':
+    main()
